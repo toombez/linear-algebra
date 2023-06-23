73c11b5dbc8607f24c6590c8bdabc938cdd8b8bc
Timur <timurtokaev.toombez@gmail.com>
2023-06-24 01:31:14 +0300

    Implement normalize

--- packages/vector/src/NumberVector.ts
+++ packages/vector/src/NumberVector.ts
@@ -5,6 +5,14 @@ export default class NumberVector
     extends Vector<number>
     implements IMathOperationsVector<number>
 {
+    public get magnitude() {
+        const sumOfSquares = this.components
+            .map((component) => component ** 2)
+            .reduce((sum, component) => sum + component, 0)
+
+        return Math.sqrt(sumOfSquares)
+    }
+
     public add(vector: VectorAsParameter<number>): NumberVector {
         const components = new NumberVector(vector).components
             .map((component, index) => component + this.components[index])
@@ -44,4 +52,12 @@ export default class NumberVector
     }
 
     // TODO: add cross product
+
+    public normalize(): NumberVector {
+        const { magnitude } = this
+        const components = this.components
+            .map((component) => component / magnitude)
+
+        return new NumberVector(components)
+    }
 }
