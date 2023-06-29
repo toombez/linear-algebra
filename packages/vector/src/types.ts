179ed4658f2d76341a81361175ebc59511e57ba2
Timur <timurtokaev.toombez@gmail.com>
2023-06-29 14:01:16 +0300

    Add immutable vector type

--- packages/vector/src/types.ts
+++ packages/vector/src/types.ts
@@ -4,19 +4,17 @@
 export type VectorType<T = unknown> = Array<T>
 
 /**
- * One dimensions `vector` type
+ * Immutable vector type
  */
-export type Vector1DType<T = unknown> = VectorType<T>
-
-/**
- * Two dimensions `vector` type
- */
-export type Vector2DType<T = unknown> = Vector1DType<Vector1DType<T>>
+export type ImmutableVectorType<T> = Readonly<VectorType<T>>
 
 /**
  * Vector type to pass as parameter
  */
-export type VectorAsParameter<T> = VectorType<T> | IVector<T>
+export type VectorAsParameter<T> =
+    VectorType<T>
+    | ImmutableVectorType<T>
+    | IVector<T>
 
 /**
  * `Vector` interface for implementing any `vector` class
